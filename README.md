e4ea38598aa8fac510d57bba72f7079104aa8137
Baptouuuu <baptouuuu@BBC-16.local>
2026-01-17 16:29:39 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Time
+
+[![Build Status](https://github.com/Innmind/time/actions/workflows/ci.yml/badge.svg)](https://github.com/Innmind/time/actions/workflows/ci.yml)
+[![codecov](https://codecov.io/gh/innmind/time/branch/develop/graph/badge.svg)](https://codecov.io/gh/innmind/time)
+[![Type Coverage](https://shepherd.dev/github/innmind/time/coverage.svg)](https://shepherd.dev/github/innmind/time)
+
+Description
+
+## Installation
+
+```sh
+composer require innmind/time
+```
+
+## Usage
+
+Todo
---
+++ blackbox.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types = 1);
+
+require 'vendor/autoload.php';
+
+use Innmind\BlackBox\{
+    Application,
+    Runner\Load,
+    Runner\CodeCoverage,
+};
+
+Application::new($argv)
+->when(
+        \getenv('BLACKBOX_SET_SIZE') !== false,
+        static fn(Application $app) => $app->scenariiPerProof((int) \getenv('BLACKBOX_SET_SIZE')),
+    )
+    ->when(
+        \get_env('ENABLE_COVERAGE') !== false,
+        static fn(Application $app) => $app 
+            ->scenariiPerProof(1)
+            ->codeCoverage(
+                CodeCoverage::of(
+                    __DIR__.'/src/',
+                    __DIR__.'/proofs/',
+                )
+                    ->dumpTo('coverage.clover')
+                    ->enableWhen(true),
+            ),
+    )
+    ->tryToProve(Load::everythingIn(__DIR__.'/proofs/'))
+    ->exit();
